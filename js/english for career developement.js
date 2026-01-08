f7a47caf7a9419f018010cd4a7260e9d55b09dde
Dipti17Bhawar <diptibhawar@gmail.com>
2026-01-08 15:33:09 +0530

    js logic  for english

---
+++ js/english for career developement.js
@@ -0,0 +1,17 @@
+function showSection(id) {
+  document.querySelectorAll('.section').forEach(sec => {
+    sec.classList.remove('active');
+  });
+
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  document.getElementById(id).classList.add('active');
+  event.target.classList.add('active');
+}
+
+function toggleAccordion(header) {
+  const body = header.nextElementSibling;
+  body.style.display = body.style.display === "block" ? "none" : "block";
+}
